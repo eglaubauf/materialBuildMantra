a0ec265455093b9f3deb6629724cf3ae55f9af17
Elmar <elmar@glaubauf.at>
2019-09-19 19:20:12 +0200

    Object Oriented Version

--- scripts/eg_convertToRat.py
+++ scripts/eg_convertToRat.py
@@ -2,7 +2,7 @@
 #LICENSE                            #
 #####################################
 #
-# Copyright (C) 2017  Elmar Glaubauf
+# Copyright (C) 2019  Elmar Glaubauf
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -39,6 +39,17 @@ extension = ".rat"
 ############END CONFIG ############
 
 
+def run():
+    
+    nodes = hou.selectedNodes()
+
+    c = Converter(nodes)
+    #Print Result
+    c.finish()
+
+############################
+
+
 #HelperClass
 class Entry:
     texPath = ""
@@ -50,94 +61,104 @@ class Entry:
         self.node = node
         self.parm = parm
 
-myList = []
-errors = []
+#Converter Class
+class Converter():
 
+    def __init__(self, nodes):
+        self.textures = []
+        self.errors = []
+        self.count = 0
+        self.nodes = nodes
 
-def run():
-
-    #mat = hou.node("/mat")
-    #nodes = mat.children()
-    
-    nodes = hou.selectedNodes()
-    changeMats(nodes)
-
-    convertFiles()
-
-    text = str(len(myList)) + " Textures converted. \n "
-    
-    if errors: 
-        text = printErrors(text)
-    hou.ui.displayMessage(text)
-
-
-def changeMats(nodes):
-    global myList
-    for n in nodes:
-        if n.type().name() == "texture::2.0" or n.type().name() == "principledshader::2.0" or n.type().name() == "displacetexture" :
-            for p in n.parms():
-                if p.parmTemplate().type() == hou.parmTemplateType.String:
-                    # Get Parm as String
-                    s = p.evalAsString()
-                    occur = s.rfind("/")
-                    #Filter out non Files - go to next if none
-                    if occur == -1:
-                        continue
-                    if s.startswith(hou.getenv("HSITE")):   #Leave if already in place
-                        continue
-                    if not os.path.exists(s):
-                        #global errors
-                        s = (s + " does not exist")
-                        errors.append(s)
-                        continue
-
-                    #Append string to Converted List
-                    if not s.endswith('.rat'):
-                        myList.append(Entry(s, n, p))
-
-
-        if n.type().name() == "materialbuilder":
-            children = n.children() 
-            changeMats(children)
-
-
-def convertFiles():
-    global myList
-    cop = hou.node("/img")
-    img = cop.createNode("img", "tmp")
-    
-    read = img.createNode("file")
-    rop = img.createNode("rop_comp")
-    rop.setInput(0,read,0)
-    rop.parm("trange").set(0)
-
-    #Convert Stuff
-    for f in myList:
-        filename = f.texPath
-        read.parm("filename1").set(filename)
+        #Convert stuff
+        self.gatherTextures()
+        self.convertFiles()
         
-        #Change Filename
-        namePos = filename.rfind(".")
-        name = filename[:namePos]
-        name += extension
+        return
+    
+    #Returns Count
+    def getCount(self):
+        return self.count
 
-        #Render
-        rop.parm("copoutput").set(name)
-        rop.parm("execute").pressButton()
+    #Gathers Textures to be changed
+    def gatherTextures(self):
         
-        #Change Original Material/Texture
-        f.node.parm(f.parm.name()).set(name)
-    #Cleanup
-    img.destroy()
+        for n in self.nodes:
+            if n.type().name() == "texture::2.0" or n.type().name() == "principledshader::2.0" or n.type().name() == "displacetexture" :
+                for p in n.parms():
+                    if p.parmTemplate().type() == hou.parmTemplateType.String:
+                        # Get Parm as String
+                        s = p.evalAsString()
+                        occur = s.rfind("/")
+                        #Filter out non Files - go to next if none
+                        if occur == -1:
+                            continue
+                        if s.startswith(hou.getenv("HSITE")):   #Leave if already in place
+                            continue
+                        if not os.path.exists(s):
+                            #global errors
+                            s = (s + " does not exist \n")
+                            self.errors.append(s)
+                            continue
+
+                        #Append string to Converted List
+                        if not s.endswith('.rat'):
+                            self.textures.append(Entry(s, n, p))
+
+            if n.type().name() == "materialbuilder":
+                #Create a new Instance of this Class for SubChildren
+                ch = Converter(n.children())
+                self.count += ch.getCount()
 
 
+    #Converts Files
+    def convertFiles(self):
+        
+        cop = hou.node("/img")
+        img = cop.createNode("img", "tmp")
+        
+        read = img.createNode("file")
+        rop = img.createNode("rop_comp")
+        rop.setInput(0,read,0)
+        rop.parm("trange").set(0)
 
-def printErrors(text):
+        #Convert Stuff
+        for f in self.textures:
+            filename = f.texPath
+            read.parm("filename1").set(filename)
+            
+            #Change Filename
+            namePos = filename.rfind(".")
+            name = filename[:namePos]
+            name += extension
 
-    text = text + "Errors: \n"
-    msg = ""
-    for s in errors:
-        msg = msg + s + "\n"
+            #Render
+            rop.parm("copoutput").set(name)
+            rop.parm("execute").pressButton()
+            
+            #Change Original Material/Texture
+            f.node.parm(f.parm.name()).set(name)
 
-    text = text + msg
-    return text
+            self.count += 1
+
+        #Cleanup 
+        img.destroy()
+
+    #Cleanup after Converting
+    def finish(self):
+        text = str(self.count) + " Textures converted. \n "
+        
+        if errors: 
+            text = self.printErrors(text)
+        hou.ui.displayMessage(text)
+
+    #Print Errors if happend - no exception to continue other files
+    def printErrors(text):
+
+        text = text + "Errors: \n"
+        msg = ""
+        for s in errors:
+            msg = msg + s + "\n"
+
+        text = text + msg
+        return text
